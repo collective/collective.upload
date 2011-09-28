7f0e3a4d0b09596d2cb6e5a1cdbf81de210c2282
hvelarde <hector.velarde@gmail.com>
2011-09-28 17:31:53 -0300

    added initial package structure

---
+++ README.txt
@@ -0,0 +1,4 @@
+Introduction
+============
+
+An implementation of the jQuery File Upload Plugin for Plone.
---
+++ setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+import os
+
+version = '1.0dev'
+
+setup(name='collective.upload',
+      version=version,
+      description="An implementation of the jQuery File Upload Plugin for Plone.",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        ],
+      keywords='plone jquery',
+      author='Joaquín Rosales',
+      author_email='globojorro@gmail.com',
+      url='https://github.com/collective/collective.upload',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+        'setuptools',
+        'five.grok',
+        ],
+      extras_require={
+        'test': ['plone.app.testing'],
+        },
+      entry_points="""
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
