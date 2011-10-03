51246b55f91d2f1518769ee8d0fd7b6162deca75
hvelarde <hector.velarde@gmail.com>
2011-10-03 14:59:15 -0300

    added buildout to package

---
+++ rebuild_i18n.sh
@@ -0,0 +1,14 @@
+#! /bin/sh
+
+I18NDOMAIN="collective.upload"
+
+# Synchronise the templates and scripts with the .pot.
+# All on one line normally:
+bin/i18ndude rebuild-pot --pot src/collective/upload/locales/${I18NDOMAIN}.pot \
+    --create ${I18NDOMAIN} \
+   .
+
+# Synchronise the resulting .pot with all .po files
+for po in src/collective/upload/locales/*/LC_MESSAGES/${I18NDOMAIN}.po; do
+    bin/i18ndude sync --pot src/collective/upload/locales/${I18NDOMAIN}.pot $po
+done
