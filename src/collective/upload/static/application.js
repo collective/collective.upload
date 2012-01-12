4d4e7db3ac797f2251df9c331f8db791cb15eeb3
Silvestre Huens <s.huens@gmail.com>
2012-01-12 05:49:05 -0300

    removing jquery 1.7.1

--- src/collective/upload/static/application.js
+++ src/collective/upload/static/application.js
@@ -11,8 +11,8 @@
 
 /*jslint nomen: true, unparam: true, regexp: true */
 /*global $, window, document */
-
-$.widget('blueimpUIX.fileupload', $.blueimpUI.fileupload, {
+(function ($) {
+jQuery.widget('blueimpUIX.fileupload', jQuery.blueimpUI.fileupload, {
 
     _renderTemplate: function (func, files) {
         return func({
@@ -84,65 +84,73 @@ $.widget('blueimpUIX.fileupload', $.blueimpUI.fileupload, {
     }
 
 });
+}(jQuery));
 
 
-$(document).ready(function() {
+jQuery(document).ready(function() {
+    var $ = jQuery;
   // Handler for .ready() called.
     config_upload_form  = function () {
     'use strict';
 
-    // Initialize the jQuery File Upload widget:
-    $('#fileupload').fileupload(
-        {
-            'sequentialUploads':true,
-            dragover: function (e, data) {
+    //we have to check if the fileupload element existing
+    
+    if ($('#fileupload')[0] != undefined) {
+        // Initialize the jQuery File Upload widget:
+        
+        $('#fileupload').fileupload(
+            {
+                'sequentialUploads':true,
+                dragover: function (e, data) {
+                }
             }
-        }
-    )
+        )
 
-    // Load existing files:
-    $.getJSON($('#fileupload').prop('action'), function (files) {
-        var fu = $('#fileupload').data('fileupload'),
-            template;
-        fu._adjustMaxNumberOfFiles(-files.length);
-        template = fu._renderDownload(files)
-            .appendTo($('#fileupload .files'));
-        // Force reflow:
-        fu._reflow = fu._transition && template.length &&
-            template[0].offsetWidth;
-        template.addClass('in');
-    });
+        // Load existing files:
+        $.getJSON($('#fileupload').prop('action'), function (files) {
+            var fu = $('#fileupload').data('fileupload'),
+                template;
+            fu._adjustMaxNumberOfFiles(-files.length);
+            template = fu._renderDownload(files)
+                .appendTo($('#fileupload .files'));
+            // Force reflow:
+            fu._reflow = fu._transition && template.length &&
+                template[0].offsetWidth;
+            template.addClass('in');
+        });
 
 
-    // Enable iframe cross-domain access via redirect page:
-    var redirectPage = window.location.href.replace(
-        /\/[^\/]*$/,
-        '/result.html?%s'
-    );
-    $('#fileupload').bind('fileuploadsend', function (e, data) {
-        if (data.dataType.substr(0, 6) === 'iframe') {
-            var target = $('<a/>').prop('href', data.url)[0];
-            if (window.location.host !== target.host) {
-                data.formData.push({
-                    name: 'redirect',
-                    value: redirectPage
-                });
+        // Enable iframe cross-domain access via redirect page:
+        var redirectPage = window.location.href.replace(
+            /\/[^\/]*$/,
+            '/result.html?%s'
+        );
+        $('#fileupload').bind('fileuploadsend', function (e, data) {
+            if (data.dataType.substr(0, 6) === 'iframe') {
+                var target = $('<a/>').prop('href', data.url)[0];
+                if (window.location.host !== target.host) {
+                    data.formData.push({
+                        name: 'redirect',
+                        value: redirectPage
+                    });
+                }
             }
-        }
-    });
+        });
 
-    // Open download dialogs via iframes,
-    // to prevent aborting current uploads:
-    $('#fileupload .files').delegate(
-        'a:not([rel^=gallery])',
-        'click',
-        function (e) {
-            e.preventDefault();
-            $('<iframe style="display:none;"></iframe>')
-                .prop('src', this.href)
-                .appendTo(document.body);
-        }
-    );
+        // Open download dialogs via iframes,
+        // to prevent aborting current uploads:
+        $('#fileupload .files').delegate(
+            'a:not([rel^=gallery])',
+            'click',
+            function (e) {
+                e.preventDefault();
+                $('<iframe style="display:none;"></iframe>')
+                    .prop('src', this.href)
+                    .appendTo(document.body);
+            }
+        );
+        
+    };
 
 };
 config_upload_form();
