baa0b07200e2c69f7cb0dd795568cc9a120d8859
hvelarde <hector.velarde@gmail.com>
2012-05-01 17:23:57 -0300

    control panel refactoring: pep8/pyflakes; default values were moved to config.py (they are now big enough not to interfere with normal operation at the very beginning)

--- src/collective/upload/controlpanel.py
+++ src/collective/upload/controlpanel.py
@@ -1,20 +1,23 @@
+# -*- coding: utf-8 -*-
+
 from plone.app.registry.browser import controlpanel
 
-from collective.upload.interfaces import IUploadSettings, _
+from collective.upload import _
+from collective.upload.interfaces import IUploadSettings
 
 
 class UploadSettingsEditForm(controlpanel.RegistryEditForm):
 
     schema = IUploadSettings
     label = _(u"Upload settings")
-    description = _(u"""""")
+    description = _(u"Here you can modify the settings for collective.upload")
 
     def updateFields(self):
         super(UploadSettingsEditForm, self).updateFields()
 
-
     def updateWidgets(self):
         super(UploadSettingsEditForm, self).updateWidgets()
+
 
 class UploadSettingsControlPanel(controlpanel.ControlPanelFormWrapper):
     form = UploadSettingsEditForm
--- src/collective/upload/interfaces.py
+++ src/collective/upload/interfaces.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
+
 from zope import schema
 from zope.interface import Interface
 
-from zope.i18nmessageid import MessageFactory
 from plone.theme.interfaces import IDefaultPloneLayer
 
-_ = MessageFactory('collective.upload')
+from collective.upload import _
+from collective.upload import config
 
 
 class IUploadBrowserLayer(IDefaultPloneLayer):
@@ -18,29 +19,37 @@ class IUploadSettings(Interface):
     configuration registry and obtainable via plone.registry.
     """
 
-    upload_extensions = schema.TextLine(title=_(u"Allowed Extensions"),
-                                         description=_(u"allowed_extensions_key",
-                                         default=u"List the file extensions allowed for upload, "
-                                                  "without dot, comma separated"),
-                                         required=False,
-                                         default=u'gif, jpg, jpeg, png',)
+    upload_extensions = schema.TextLine(
+        title=_(u"Allowed Extensions"),
+        description=_(u"allowed_extensions_key",
+        default=u"List the file extensions allowed for upload, "
+                u"without dot, comma separated"),
+        required=False,
+        default=config.UPLOAD_EXTENSIONS,
+        )
 
-    max_file_size = schema.Int(title=_(u"Max file size"),
-                                         description=_(u"max_file_size_key",
-                                         default=u"The maximum allowed file size in bytes "
-                                                  "ej: 10000000 ~ 10mb"),
-                                         required=False,
-                                         default=10000000,)
+    max_file_size = schema.Int(
+        title=_(u"Max file size"),
+        description=_(u"max_file_size_key",
+                      default=u"The maximum allowed file size in bytes. "
+                              u"Please note that intermediate web servers "
+                              u"may put limits on this value; ask your "
+                              u"Systems Administrator in case of doubt."),
+        required=False,
+        default=config.MAX_FILE_SIZE,)
 
-    resize_max_width = schema.Int(title=_(u"maximum width"),
-                                         description=_(u"resize_max_width_key",
-                                                       default=u"The maximum width for uploaded images."),
-                                         required=False,
-                                         default=1000,
-                                         )
-    resize_max_height = schema.Int(title=_(u"maximum height"),
-                                         description=_(u"resize_max_height_key",
-                                                       default=u"The maximum height for uploaded images."),
-                                         required=False,
-                                         default=1000,
-                                         )
+    resize_max_width = schema.Int(
+        title=_(u"maximum width"),
+        description=_(u"resize_max_width_key",
+                      default=u"The maximum width for uploaded images."),
+        required=False,
+        default=config.RESIZE_MAX_WIDTH,
+        )
+
+    resize_max_height = schema.Int(
+        title=_(u"maximum height"),
+        description=_(u"resize_max_height_key",
+                      default=u"The maximum height for uploaded images."),
+        required=False,
+        default=config.RESIZE_MAX_HEIGHT,
+        )
