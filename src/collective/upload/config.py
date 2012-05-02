07bae8926f1a202108c02183da950c8203a26f0f
hvelarde <hector.velarde@gmail.com>
2012-05-02 19:28:05 -0300

    fixed MAX_FILE_SIZE value; it was 10KB instead of 10MB

--- src/collective/upload/config.py
+++ src/collective/upload/config.py
@@ -4,10 +4,6 @@ PROJECTNAME = 'collective.upload'
 
 # XXX: we don't want to put limits here; that should be user configurable
 UPLOAD_EXTENSIONS = u'gif, jpeg, jpg, png'
-
-# XXX: that should be big enough
-MAX_FILE_SIZE = 10 * 1024  # 10MB
-
-# XXX: the following values are for images of aprox 10 MP
-RESIZE_MAX_WIDTH = 3872
-RESIZE_MAX_HEIGHT = 3872
+MAX_FILE_SIZE = 10 * 1024 * 1024  # 10MB; that should be big enough
+RESIZE_MAX_WIDTH = 3872  # 10 MP landscape
+RESIZE_MAX_HEIGHT = 3872  # 10 MP portrait
