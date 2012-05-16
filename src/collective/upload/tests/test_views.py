91e173dd43d1a5c15926707e53fd0a8bcef8eef5
hvelarde <hector.velarde@gmail.com>
2012-05-16 16:56:40 -0300

    added incomplete test case for API view

--- src/collective/upload/tests/test_views.py
+++ src/collective/upload/tests/test_views.py
@@ -37,3 +37,44 @@ class JSVariablesViewTestCase(unittest.TestCase):
         expected = "{'max_file_size': 10485760, 'resize_max_width': 3872, " \
                    "'extensions': 'gif|jpeg|jpg|png', 'resize_max_height': 3872}"
         self.assertEqual(registry_config, expected)
+
+
+class APITestCase(unittest.TestCase):
+
+    layer = INTEGRATION_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+        self.request = self.layer['request']
+        directlyProvides(self.request, IUploadBrowserLayer)
+
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+        self.portal.invokeFactory('Folder', 'test-folder')
+        setRoles(self.portal, TEST_USER_ID, ['Member'])
+        self.folder = self.portal['test-folder']
+
+    def test_api_view_is_present(self):
+        view = queryMultiAdapter((self.folder, self.request), name='api')
+        self.assertTrue(view is not None)
+
+    def test_dumps(self):
+        view = getMultiAdapter((self.folder, self.request), name='api')
+
+        self.assertEqual(view.dumps(), '{}')
+        # TODO: test with content added
+
+    def test_getContextInfo(self):
+        #view = getMultiAdapter((self.folder, self.request), name='api')
+        # TODO
+        NotImplemented
+
+    def test_getContainerInfo(self):
+        #view = getMultiAdapter((self.folder, self.request), name='api')
+        # TODO
+        NotImplemented
+
+    def test_render(self):
+        view = getMultiAdapter((self.folder, self.request), name='api')
+
+        self.assertEqual(view.render(), '[]')
+        # TODO: test with content added
