748708d36934be5d773fc69213302599071165b0
quimera <s.huens@gmail.com>
2013-01-03 18:34:30 -0300

    checking the allowed ct in the context, if file or image is there, show the multiple upload option.

--- src/collective/upload/menu.py
+++ src/collective/upload/menu.py
@@ -5,6 +5,7 @@ from plone.app.contentmenu.menu import FactoriesMenu as BaseMenu
 from plone.app.contentmenu.menu import FactoriesSubMenuItem as BaseMenuItem
 from plone.app.contentmenu.interfaces import IFactoriesSubMenuItem
 from zope.interface import implements
+from zope.component import getMultiAdapter
 
 
 class FactoriesSubMenuItem(BaseMenuItem):
@@ -20,18 +21,24 @@ class FactoriesMenu(BaseMenu):
     def getMenuItems(self, context, request):
         # menuitems is a list of tal-friendly dictionaries
         menuitems = super(FactoriesMenu, self).getMenuItems(context, request)
+        allowed_content_types = ['File', 'Image']
 
-        url = context.absolute_url()
-        new_menu = {'extra': {'separator': None,
-                              'id': 'multiple-files',
-                              'class': 'contenttype-multiple-files'},
-                    'submenu': None,
-                    'description': _(u'A form to upload multiple files.'),
-                    'title': _(u'Multiple Files'),
-                    'action': '%s/@@media_uploader' % url,
-                    'selected': False,
-                    'id': 'Multiple Files',
-                    'icon': ICON}
-        menuitems.insert(-1, new_menu)
+        context_state = getMultiAdapter((context, request), name='plone_context_state')
+
+        obj = context_state.folder()
+
+        if set([x.id for x in obj.allowedContentTypes()]) & set(allowed_content_types):
+            url = obj.absolute_url()
+            new_menu = {'extra': {'separator': None,
+                                  'id': 'multiple-files',
+                                  'class': 'contenttype-multiple-files'},
+                        'submenu': None,
+                        'description': _(u'A form to upload multiple files.'),
+                        'title': _(u'Multiple Files'),
+                        'action': '%s/@@media_uploader' % url,
+                        'selected': False,
+                        'id': 'Multiple Files',
+                        'icon': ICON}
+            menuitems.insert(-1, new_menu)
 
         return menuitems
