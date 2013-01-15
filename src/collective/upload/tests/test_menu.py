c6c6e398a20a8909049272ac30fe162d44dad096
hvelarde <hector.velarde@gmail.com>
2013-01-15 17:04:08 -0200

    fix test

--- src/collective/upload/tests/test_menu.py
+++ src/collective/upload/tests/test_menu.py
@@ -29,7 +29,7 @@ class MenuFunctionalTest(unittest.TestCase):
         self.assertIn('@@media_uploader', browser.contents)
         browser.getLink('Multiple Files').click()
         self.assertIn('@@media_uploader', browser.url)
-        self.assertIn('Add files or images…', browser.contents)
+        self.assertIn('Add files or images&hellip;', browser.contents)
 
 
 class MenuIntegrationTest(unittest.TestCase):
