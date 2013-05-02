5ca943ae73f9ff871c10581c9ff40359947218a3
hvelarde <hector.velarde@gmail.com>
2013-05-02 10:54:27 -0300

    use correct .rst extension on documentation files

--- docs/INSTALL.rst
+++ docs/INSTALL.rst
@@ -11,12 +11,12 @@ To enable this product in a buildout-based installation:
     eggs =
         collective.upload
 
-2. You may need to pin the right versions of the jQuery and jQuery Tools
-   packages::
+2. If you're using Plone 4.2, you may need to pin the right versions of the
+   jQuery and jQuery Tools packages::
 
     [versions]
     plone.app.jquery = 1.7.2
-    plone.app.jquerytools = 1.5.1
+    plone.app.jquerytools = 1.5.5
 
 After updating the configuration you need to run ''bin/buildout'', which will
 take care of updating your system.
--- setup.py
+++ setup.py
@@ -6,10 +6,12 @@ import os
 version = '1.0b4.dev0'
 description = "File upload widget with multiple file selection, drag&drop \
 support, progress bars, client-side image resizing and preview images."
-long_description = open("README.txt").read() + "\n" + \
-    open(os.path.join("docs", "INSTALL.txt")).read() + "\n" + \
-    open(os.path.join("docs", "CREDITS.txt")).read() + "\n" + \
-    open(os.path.join("docs", "HISTORY.txt")).read()
+long_description = (
+    open('README.rst').read() + '\n' +
+    open(os.path.join('docs', 'INSTALL.rst')).read() + '\n' +
+    open(os.path.join('docs', 'CREDITS.rst')).read() + '\n' +
+    open(os.path.join('docs', 'HISTORY.rst')).read()
+)
 
 setup(name='collective.upload',
       version=version,
