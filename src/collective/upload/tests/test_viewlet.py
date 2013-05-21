d13abf9bd7441e9eb39bd7cbbb879050756fb686
Érico Andrei <erico@simplesconsultoria.com.br>
2013-05-21 12:30:41 -0300

    Display viewlet **only** for users with Add portal content permission and when context is a container

--- src/collective/upload/tests/test_viewlet.py
+++ src/collective/upload/tests/test_viewlet.py
@@ -8,11 +8,14 @@ from zope.viewlet.interfaces import IViewletManager
 
 from plone.app.testing import TEST_USER_ID
 from plone.app.testing import setRoles
+from plone.app.testing import logout
 
 from Products.Five.browser import BrowserView as View
 
 from collective.upload.behaviors import IMultipleUpload
 from collective.upload.testing import INTEGRATION_TESTING
+
+NAME = 'collective.upload.tmpls'
 
 
 class ViewletTest(unittest.TestCase):
@@ -30,12 +33,46 @@ class ViewletTest(unittest.TestCase):
         # HACK: our Folder object "has" the behavior just for testing purposes
         alsoProvides(self.folder, IMultipleUpload)
 
-    def test_viewlet_is_present(self):
+    def test_manager_viewlet_is_present_in_folder(self):
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
         view = View(self.folder, self.request)
         manager = queryMultiAdapter((self.folder, self.request, view),
                                     IViewletManager, 'plone.htmlhead')
         self.assertTrue(manager is not None)
 
         manager.update()
-        viewlet = [v for v in manager.viewlets if v.__name__ == 'collective.upload.tmpls']
+        viewlet = [v for v in manager.viewlets if v.__name__ == NAME]
         self.assertEqual(len(viewlet), 1)
+
+    def test_manager_viewlet_is_present_in_portal(self):
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+        view = View(self.portal, self.request)
+        manager = queryMultiAdapter((self.portal, self.request, view),
+                                    IViewletManager, 'plone.htmlhead')
+        self.assertTrue(manager is not None)
+
+        manager.update()
+        viewlet = [v for v in manager.viewlets if v.__name__ == NAME]
+        self.assertEqual(len(viewlet), 1)
+
+    def test_anonymous_viewlet_is_not_present_in_folder(self):
+        logout()
+        view = View(self.folder, self.request)
+        manager = queryMultiAdapter((self.folder, self.request, view),
+                                    IViewletManager, 'plone.htmlhead')
+        self.assertTrue(manager is not None)
+
+        manager.update()
+        viewlet = [v for v in manager.viewlets if v.__name__ == NAME]
+        self.assertEqual(len(viewlet), 0)
+
+    def test_anonymous_viewlet_is_not_present_in_portal(self):
+        logout()
+        view = View(self.portal, self.request)
+        manager = queryMultiAdapter((self.portal, self.request, view),
+                                    IViewletManager, 'plone.htmlhead')
+        self.assertTrue(manager is not None)
+
+        manager.update()
+        viewlet = [v for v in manager.viewlets if v.__name__ == NAME]
+        self.assertEqual(len(viewlet), 0)
--- src/collective/upload/viewlets.py
+++ src/collective/upload/viewlets.py
@@ -1,19 +1,14 @@
 # -*- coding: utf-8 -*-
-# Zope imports
-from zope.interface import Interface
 from five import grok
-
-# Plone imports
 from plone.app.layout.viewlets.interfaces import IHtmlHead
-
+from OFS.interfaces import IFolder
 
 grok.templatedir("viewlets")
-# grok.layer(IVTVLayer)
 
 
 class Tmpls(grok.Viewlet):
-    grok.context(Interface)
+    grok.context(IFolder)
     grok.name(u"collective.upload.tmpls")
-    grok.require("zope2.View")
+    grok.require("cmf.AddPortalContent")
     grok.template("tmpls")
     grok.viewletmanager(IHtmlHead)
