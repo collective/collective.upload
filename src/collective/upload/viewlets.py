bb69ff3a2f3c70eb0314aee016f3ed5753bc17e5
Juan Pablo Giménez <jpg@rcom.com.ar>
2013-07-24 17:21:43 -0300

    Fix issue #24

--- src/collective/upload/viewlets.py
+++ src/collective/upload/viewlets.py
@@ -1,14 +1,24 @@
 # -*- coding: utf-8 -*-
 from five import grok
 from plone.app.layout.viewlets.interfaces import IHtmlHead
-from OFS.interfaces import IFolder
+from zope.interface import Interface
+from zope.component import getMultiAdapter
+from Products.CMFCore.interfaces import IFolderish
+
 
 grok.templatedir("viewlets")
 
 
 class Tmpls(grok.Viewlet):
-    grok.context(IFolder)
+    grok.context(Interface)
     grok.name(u"collective.upload.tmpls")
     grok.require("cmf.AddPortalContent")
     grok.template("tmpls")
     grok.viewletmanager(IHtmlHead)
+
+    def enabled(self):
+        context = self.context
+        context_state = getMultiAdapter((context, self.request), name=u'plone_context_state')
+        if context_state.is_default_page():
+            context = context.aq_parent
+        return IFolderish.providedBy(context)
