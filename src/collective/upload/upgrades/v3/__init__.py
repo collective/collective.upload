776b78a19221baa6da5363388b89c1bf19793892
Rodrigo Ferreira de Souza <rodfersou@gmail.com>
2016-04-07 13:55:12 -0300

    Use collective.js.bootstrap

--- src/collective/upload/upgrades/v3/__init__.py
+++ src/collective/upload/upgrades/v3/__init__.py
@@ -1,6 +1,36 @@
 # -*- coding: utf-8 -*-
 from collective.upload.config import PROJECTNAME
 from collective.upload.logger import logger
+from plone import api
+
+
+FIX_CSS = {
+    'old': '++resource++collective.upload/css/bootstrap.min.css',
+    'new': '++resource++collective.js.bootstrap/css/bootstrap.min.css'
+}
+FIX_JS = {
+    'old': '++resource++collective.upload/vendor/bootstrap.min.js',
+    'new': '++resource++collective.js.bootstrap/js/bootstrap.min.js'
+}
+
+
+def fix_resources_references(setup_tool):
+    """Fix resource references after add collective.js.bootstrap dependency."""
+    css_tool = api.portal.get_tool('portal_css')
+    css_ids = css_tool.getResourceIds()
+    if FIX_CSS['old'] in css_ids and FIX_CSS['new'] not in css_ids:
+        css_tool.renameResource(FIX_CSS['old'], FIX_CSS['new'])
+    elif FIX_CSS['old'] in css_ids and FIX_CSS['new'] in css_ids:
+        css_tool.unregisterResource(FIX_CSS['old'])
+    logger.info('Updated css references.')
+
+    js_tool = api.portal.get_tool('portal_javascripts')
+    js_ids = js_tool.getResourceIds()
+    if FIX_JS['old'] in js_ids and FIX_JS['new'] not in js_ids:
+        js_tool.renameResource(FIX_JS['old'], FIX_JS['new'])
+    elif FIX_JS['old'] in js_ids and FIX_JS['new'] in js_ids:
+        js_tool.unregisterResource(FIX_JS['old'])
+    logger.info('Updated javascript references.')
 
 
 def update_jsregistry(setup_tool):
