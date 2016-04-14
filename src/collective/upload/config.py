d2fdf0f91b4048e2dde56632fd603cc5d27b50ac
hvelarde <hector.velarde@gmail.com>
2016-04-14 15:08:40 -0300

    Fix icon location

--- src/collective/upload/config.py
+++ src/collective/upload/config.py
@@ -9,4 +9,4 @@ MAX_FILE_SIZE = 10 * 1024 * 1024  # 10MB; that should be big enough
 RESIZE_MAX_WIDTH = 3872  # 10 MP landscape
 RESIZE_MAX_HEIGHT = 3872  # 10 MP portrait
 
-ICON = '++resource++collective.upload/img/document_small_upload.png'
+ICON = '++resource++collective.upload/upload-icon.png'
